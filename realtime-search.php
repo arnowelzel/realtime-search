5be9333564a231122853dca8f293fdbd87253a08
Arno Welzel <github@arnowelzel.de>
2020-04-11 22:51:01 +0200

    JavaScript as external asset.

--- realtime-search.php
+++ realtime-search.php
@@ -43,13 +43,20 @@ class RealtimeSearch
      */
     function enqueueScripts()
     {
-        wp_enqueue_script('jquery');
+        wp_enqueue_script(
+            'realtimesearch-frontend',
+            plugin_dir_url(__FILE__) . 'assets/frontend.min.js',
+            array('jquery'),
+            self::REALTIME_SEARCH_VERSION
+        );
         wp_enqueue_style(
             'realtimesearch-frontend',
             plugin_dir_url(__FILE__) . 'assets/frontend.css',
             false,
             self::REALTIME_SEARCH_VERSION
         );
+        $translation_array['ajaxurl'] = admin_url('admin-ajax.php');
+        wp_localize_script('realtimesearch-frontend', 'realtimesearch_options', $translation_array);
     }
 
     /**
@@ -58,139 +65,8 @@ class RealtimeSearch
      * @return nothing
      */
     function footer()
-    { ?>
-        <div id="realTimeSearchResult"></div>
-        <script type="text/javascript">
-            var realTimeSearchTimer = null;
-            var realTimeSearchWaiting = false;
-            var realTimeSearchText = '';
-            var realTimeSearchActive = false;
-
-            function realTimeSearchSearchAjax() {
-                let searchInput = document.getElementById('realTimeSearchSearchInput');
-                let text = searchInput.value;
-                if (realTimeSearchText == text) {
-                    return;
-                }
-                if (text == '') {
-                    document.getElementById('realTimeSearchResult').style.display = 'none';
-                    realTimeSearchActive = false;
-                    window.clearInterval(realTimeSearchTimer);
-                } else {
-                    if (!realTimeSearchWaiting) {
-                        realTimeSearchText = text;
-                        realTimeSearchWaiting = true;
-                        jQuery.ajax({
-                            type: 'POST',
-                            url: '<?php echo admin_url('admin-ajax.php'); ?>',
-                            data: {
-                                action: 'realTimeSearch_search',
-                                text: text
-                            },
-                            success: function (data, textStatus, XMLHttpRequest) {
-                                realTimeSearchTimer = null;
-                                let searchInput = document.getElementById('realTimeSearchSearchInput');
-                                let searchResult = document.getElementById('realTimeSearchResult');
-                                let bounds = searchInput.getBoundingClientRect();
-                                searchResult.innerHTML = data;
-                                let left = (bounds.left - 250);
-                                if (left < 0) {
-                                    left = 0;
-                                }
-                                searchResult.style.left = left + 'px';
-                                searchResult.style.top = (bounds.top + bounds.height + window.scrollY) + 'px';
-                                searchResult.style.width = (bounds.width + 250) + 'px';
-                                searchResult.style.display = 'block';
-                                let listEntry = searchResult.firstElementChild.firstElementChild;
-                                while (listEntry) {
-                                    listEntry.firstElementChild.addEventListener('keydown', event => {
-                                        let entry = event.target.parentElement;
-                                        const realTimeSearchResult = document.getElementById('realTimeSearchResult');
-                                        if (event.keyCode == 40 && realTimeSearchActive) {
-                                            if (entry.nextElementSibling) {
-                                                entry.nextElementSibling.firstElementChild.focus();
-                                            } else {
-                                                document.getElementById('realTimeSearchSearchInput').focus();
-                                            }
-                                            event.preventDefault();
-                                        } else if (event.keyCode == 38 && realTimeSearchActive) {
-                                            if (entry.previousElementSibling) {
-                                                entry.previousElementSibling.firstElementChild.focus();
-                                            } else {
-                                                document.getElementById('realTimeSearchSearchInput').focus();
-                                            }
-                                            event.preventDefault();
-                                        } else if (event.keyCode == 27) {
-                                            realTimeSearchResult.style.display = 'none';
-                                            event.preventDefault();
-                                        }
-                                    });
-                                    listEntry = listEntry.nextElementSibling;
-                                }
-                                realTimeSearchWaiting = false;
-                                realTimeSearchActive = true;
-                            },
-                            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                                realTimeSearchWaiting = false;
-                                console.log('realTimeSearchSearch error: ' + errorThrown)
-                            }
-                        });
-                    }
-                }
-            }
-
-            function realTimeSearchSearch() {
-                if (realTimeSearchTimer != null) {
-                    window.clearInterval(realTimeSearchTimer);
-                }
-
-                realTimeSearchTimer = window.setInterval('realTimeSearchSearchAjax()', 250);
-            }
-
-            jQuery(document).ready(function () {
-                document.getElementById('realTimeSearchSearchInput').setAttribute('autocomplete', 'off');
-
-                document.addEventListener('click', (evt) => {
-                    const realTimeSearchSearchInput = document.getElementById('realTimeSearchSearchInput');
-                    const realTimeSearchResult = document.getElementById('realTimeSearchResult');
-                    let targetElement = evt.target;
-
-                    do {
-                        if (targetElement == realTimeSearchResult || targetElement == realTimeSearchSearchInput) {
-                            return;
-                        }
-                        targetElement = targetElement.parentNode;
-                    } while (targetElement);
-
-                    realTimeSearchResult.style.display = 'none';
-                });
-
-                document.getElementById('realTimeSearchSearchInput').addEventListener('focus', (event) => {
-                    if (realTimeSearchActive) {
-                        document.getElementById('realTimeSearchResult').style.display = 'block';
-                    }
-                });
-
-                document.getElementById('realTimeSearchSearchInput').addEventListener('keydown', (event) => {
-                    const realTimeSearchResult = document.getElementById('realTimeSearchResult');
-                    if (event.keyCode == 40 && realTimeSearchActive && !realTimeSearchWaiting) {
-                        realTimeSearchResult.style.display = 'block';
-                        realTimeSearchResult.firstElementChild.firstElementChild.firstElementChild.focus();
-                        event.preventDefault();
-                    } else if (event.keyCode == 38 && realTimeSearchActive && !realTimeSearchWaiting) {
-                        realTimeSearchResult.style.display = 'block';
-                        realTimeSearchResult.firstElementChild.lrealTimeSearchElementChild.firstElementChild.focus();
-                        event.preventDefault();
-                    } else if (event.keyCode == 27 && realTimeSearchActive && !realTimeSearchWaiting) {
-                        realTimeSearchResult.style.display = 'none';
-                        event.preventDefault();
-                    } else {
-                        realTimeSearchSearch();
-                    }
-                });
-            })
-        </script>
-        <?php
+    {
+        echo '<div id="realTimeSearchResult"></div>';
     }
 
     function ajaxSearch()
